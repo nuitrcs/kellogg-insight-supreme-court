84afe9ca012bd116be869335f849a5c634bfbee9
frankelavsky <frankelavsky@gmail.com>
2018-08-31 13:52:34 -0500

    added data

---
+++ scripts/data/data.js
@@ -0,0 +1,219 @@
+tilde.data = [
+	{
+		id: "Gorsuch",
+		fname:'Neil Gorsuch',
+		wiki:'https://en.wikipedia.org/wiki/Neil_Gorsuch',
+		appointed:'r'
+	},
+	{
+		id: "Kagan",
+		fname:'Elena Kagan',
+		wiki:'https://en.wikipedia.org/wiki/Elena_Kagan',
+		appointed:'d'
+	},
+	{
+		id: "Sotomayor",
+		fname:'Sonia Sotomayor',
+		wiki:'https://en.wikipedia.org/wiki/Sonia_Sotomayor',
+		appointed:'d'
+	},
+	{
+		id: "Alito",
+		fname:'Samuel Alito',
+		wiki:'https://en.wikipedia.org/wiki/Samuel_Alito',
+		appointed:'r'
+	},
+	{
+		id: "Roberts",
+		fname:'John Roberts',
+		wiki:'https://en.wikipedia.org/wiki/John_Roberts',
+		appointed:'r'
+	},
+	{
+		id: "Breyer",
+		fname:'Stephen Breyer',
+		wiki:'https://en.wikipedia.org/wiki/Stephen_Breyer',
+		appointed:'d'
+	},
+	{
+		id: "Ginsburg",
+		fname:'Ruth Bader Ginsburg',
+		wiki:'https://en.wikipedia.org/wiki/Ruth_Bader_Ginsburg',
+		appointed:'d'
+	},
+	{
+		id: "Thomas",
+		fname:'Clarence Thomas',
+		wiki:'https://en.wikipedia.org/wiki/Clarence_Thomas',
+		appointed:'r'
+	},
+	{
+		id: "Souter",
+		fname:'David Souter',
+		wiki:'https://en.wikipedia.org/wiki/David_Souter',
+		appointed:'r'
+	},
+	{
+		id: "Kennedy",
+		fname:'Anthony Kennedy',
+		wiki:'https://en.wikipedia.org/wiki/Anthony_Kennedy',
+		appointed:'r'
+	},
+	{
+		id: "Scalia",
+		fname:'Antonin Scalia',
+		wiki:'https://en.wikipedia.org/wiki/Antonin_Scalia',
+		appointed:'r'
+	},
+	{
+		id: "Rehnquist",
+		fname:'William Rehnquist',
+		wiki:'https://en.wikipedia.org/wiki/William_Rehnquist',
+		appointed:'r'
+	},
+	{
+		id: "O_Connor",
+		name: "O'Connor",
+		fname:"Sandra Day O'Connor",
+		wiki:'https://en.wikipedia.org/wiki/Sandra_Day_O%27Connor',
+		appointed:'r'
+	},
+	{
+		id: "Stevens",
+		fname:'John Paul Stevens',
+		wiki:'https://en.wikipedia.org/wiki/John_Paul_Stevens',
+		appointed:'r'
+	},
+	{
+		id: "Powell",
+		fname:'Lewis F. Powell Jr.',
+		wiki:'https://en.wikipedia.org/wiki/Lewis_F._Powell_Jr.',
+		appointed:'r'
+	},
+	{
+		id: "Blackmun",
+		fname:'Harry Blackmun',
+		wiki:'https://en.wikipedia.org/wiki/Harry_Blackmun',
+		appointed:'r'
+	},
+	{
+		id: "Burger",
+		fname:'Warren E. Burger',
+		wiki:'https://en.wikipedia.org/wiki/Warren_E._Burger',
+		appointed:'r'
+	},
+	{
+		id: "Marshall",
+		fname:'Thurgood Marshall',
+		wiki:'https://en.wikipedia.org/wiki/Thurgood_Marshall',
+		appointed:'d'
+	},
+	{
+		id: "Fortas",
+		fname:'Abe Fortas',
+		wiki:'https://en.wikipedia.org/wiki/Abe_Fortas',
+		appointed:'d'
+	},
+	{
+		id: "Goldberg",
+		fname:'Arthur Goldberg',
+		wiki:'https://en.wikipedia.org/wiki/Arthur_Goldberg',
+		appointed:'d'
+	},
+	{
+		id: "White",
+		fname:'Byron White',
+		wiki:'https://en.wikipedia.org/wiki/Byron_White',
+		appointed:'d'
+	},
+	{
+		id: "Stewart",
+		fname:'Potter Stewart',
+		wiki:'https://en.wikipedia.org/wiki/Potter_Stewart',
+		appointed:'r'
+	},
+	{
+		id: "Whittaker",
+		fname:'Charles Evans Whittaker',
+		wiki:'https://en.wikipedia.org/wiki/Charles_Evans_Whittaker',
+		appointed:'r'
+	},
+	{
+		id: "Brennan",
+		fname:'William J. Brennan Jr.',
+		wiki:'https://en.wikipedia.org/wiki/William_J._Brennan_Jr.',
+		appointed:'r'
+	},
+	{
+		id: "Harlan",
+		fname:'John Marshall Harlan',
+		wiki:'https://en.wikipedia.org/wiki/John_Marshall_Harlan_(born_1899)',
+		appointed:'r'
+	},
+	{
+		id: "Warren",
+		fname:'Earl Warren',
+		wiki:'https://en.wikipedia.org/wiki/Earl_Warren',
+		appointed:'r'
+	},
+	{
+		id: "Minton",
+		fname:'Sherman Minton',
+		wiki:'https://en.wikipedia.org/wiki/Sherman_Minton',
+		appointed:'d'
+	},
+	{
+		id: "Clark",
+		fname:'Tom C. Clark',
+		wiki:'https://en.wikipedia.org/wiki/Tom_C._Clark',
+		appointed:'d'
+	},
+	{
+		id: "Vinson",
+		fname:'Fred M. Vinson',
+		wiki:'https://en.wikipedia.org/wiki/Fred_M._Vinson',
+		appointed:'d'
+	},
+	{
+		id: "Burton",
+		fname:'Harold Hitz Burton',
+		wiki:'https://en.wikipedia.org/wiki/Harold_Hitz_Burton',
+		appointed:'d'
+	},
+	{
+		id: "Rutledge",
+		fname:'Wiley Blount Rutledge',
+		wiki:'https://en.wikipedia.org/wiki/Wiley_Blount_Rutledge',
+		appointed:'d'
+	},
+	{
+		id: "Jackson",
+		fname:'Robert H. Jackson',
+		wiki:'https://en.wikipedia.org/wiki/Robert_H._Jackson',
+		appointed:'d'
+	},
+	{
+		id: "Murphy",
+		fname:'Frank Murphy',
+		wiki:'https://en.wikipedia.org/wiki/Frank_Murphy',
+		appointed:'d'
+	},
+	{
+		id: "Douglas",
+		fname:'William O. Douglas',
+		wiki:'https://en.wikipedia.org/wiki/William_O._Douglas',
+		appointed:'d'
+	},
+	{
+		id: "Frankfurter",
+		fname:'Felix Frankfurter',
+		wiki:'https://en.wikipedia.org/wiki/Felix_Frankfurter',
+		appointed:'d'
+	},
+	{
+		id: "Reed",
+		fname:'Stanley Forman Reed',
+		wiki:'https://en.wikipedia.org/wiki/Stanley_Forman_Reed',
+		appointed:'d'
+	}
+]
